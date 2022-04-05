52397c5d4f4cc04672ab94629a88ed04a818a1db
henry <63601449+HenryBass@users.noreply.github.com>
2022-04-05 23:10:09 +0000

    more

--- script.js
+++ script.js
@@ -5,26 +5,41 @@ class Player {
     this.miners = 0;
     this.iron = 0;
     this.tempres = 20;
-    this.maxfuel = 200;
+    this.power = 200;
+    this.maxpower = 200;
     this.laststation;
+  }
+  goto(loc) {
+    document.getElementById("planetname").innerHTML = loc.name;
+    document.getElementById("planetdesc").innerHTML = loc.desc;
+    document.getElementById("planetlight").innerHTML = loc.sunlight;
+    document.getElementById("planetpop").innerHTML = loc.pop;
+  }
+  kill() {
+    this.power = this.maxpower;
+    boot()
   }
 }
 
+
 class Planet {
-  constructor(name, desc, yeild, powerreq, sunlight) {
+  constructor(name, desc, yeild, sunlight, pop, color) {
     this.name = name;
     this.desc = desc;
     this.yeild = yeild;
     this.sunlight = sunlight;
+    this.pop = pop + d(pop/5);
+    this.color = color
   }
   update() {
-    this.power = Player.solar * this.sunlight;
-  }
-  goto() {
-    document.getElementById(planetname).value = this.name;
-    document.getElementById(planetdesc).value = this.desc;
   }
 }
+
+Earth = new Planet("Earth", "A lush, water covered world", 1, 0.86, 1_900);
+
+Ship = new Player();
+
+Ship.laststation = Earth;
 
 document.getElementById("command").addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -42,9 +57,20 @@ function log(msg) {
   document.getElementById("console").insertAdjacentHTML(
   'beforeend', `<p>${msg}</p>`
   )
+  document.getElementById("console").scrollTop = document.getElementById("console").scrollHeight;
+}
+
+function d(n) {
+  return (Math.round(Math.random() * n)) + 1
 }
 
 function boot() {
   log("Booting...")
+  Ship.goto(Earth)
+  
+  setTimeout(function(){
+    log("Booted at: " + Ship.laststation.name)
+  }, 1000);
+  
 }
 boot()
